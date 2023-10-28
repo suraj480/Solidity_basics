7312ca75f2950a594d0aaef1eca1bf33430addd9
Suraj480 <surajmaurya991@gmail.com>
2023-10-28 16:55:33 +0530

    seventh commit

---
+++ contracts/dynamicByte.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Array {
+    bytes public b1 = "abc";
+
+    function pushElement() public {
+        b1.push("d");
+    }
+
+    function getElement(uint256 i) public view returns (bytes1) {
+        return b1[i];
+    }
+    function getlength() public view returns(uint){
+      return b1.length;
+    } 
+}
